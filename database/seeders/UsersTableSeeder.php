9965451184faad8c51200100451d6a87195d9819
Боднар Віктория Анатоліївна <asu_bodnar@voe.local>
2023-06-22 16:20:10 +0300

    a small changes

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,10 +20,18 @@ class UsersTableSeeder extends Seeder
         static $password;
         DB::table('users')->insert([
                         'name' => 'Victoria',
+                        'role_id' => 1,
                         'email' => 'asu_bodnar@voe.com.ua',
                         'password' => $password ?: $password = bcrypt('123456')
                         //'remember_token' => str_random(10),
-                    ]);
+        ]);
+        DB::table('users')->insert([
+                        'name' => 'Tamara',
+                        'role_id' => 2,
+                        'email' => 'dv4@voe.com.ua',
+                        'password' => $password ?: $password = bcrypt('123456')
+                        //'remember_token' => str_random(10),
+        ]);                 
          //factory(App\User::class, 50)->create();
 
     }
